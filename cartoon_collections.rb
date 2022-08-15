ef961c97a4ee41266c16b881a2ee09b371012d02
Ken Muyesu <ken.muyesu@student.moringaschool.com>
2022-08-15 09:09:35 +0300

    commit tree 8524262fbda4c983f45b100f0a2e622e9867c9dc

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+  dwarves.map.with_index(1) { |dwarf, number| puts "#{number}. #{dwarf}"}
 end
+#================================================================
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map { |planeteer_call| "#{planeteer_call.capitalize}!"}
 end
+#================================================================
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { |call| call.length > 4}
 end
+#================================================================
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(items)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  items.find { |is_it_cheese| cheese_types.include?(is_it_cheese)}
 end
+#================================================================
+
